cca544c31a0a968a2ba18b11db30dc57b587433b
Carmen Scholte Lubberink <Carmen@BTO.(none)>
2018-03-18 20:57:43 +0100

    Foodservice Post works for "*" CrossOrigin

--- src/main/java/csl/Application.java
+++ src/main/java/csl/Application.java
@@ -5,28 +5,19 @@ import csl.rest.FoodService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@RestController
 public class Application {
-
-    @RequestMapping("/")
-    String home() {
-        return "Simple Java Servlet by CSL";
-    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
         FoodService fc = new FoodService();
-        fc.insertFood("Banana");
+//        fc.insertFood("Banana");
         List<Food> allFood = fc.getAllFood();
         System.out.println(allFood.toString());
 
     }
-
 }
--- src/main/java/csl/rest/FoodService.java
+++ src/main/java/csl/rest/FoodService.java
@@ -2,28 +2,37 @@ package csl.rest;
 
 import csl.database.FoodRepository;
 import csl.database.model.Food;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
  * Created by Carmen on 18-3-2018.
  */
-@Controller
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/food")
 public class FoodService {
 
     private FoodRepository foodRepository = new FoodRepository();
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "",
+            method = GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Food> getAllFood() {
         return foodRepository.getAllFood();
     }
 
-    @RequestMapping(value = "/newFood/", method = RequestMethod.POST)
+    @CrossOrigin(origins = {"*", "http://localhost:4200"})
+    @RequestMapping(value = "/newFood",
+            method = POST,
+            headers = {"Content-Type=application/json"},
+            params = {"name"})
+    @ResponseBody
     public void insertFood(@RequestParam("name") String name) {
         int insertedRows = foodRepository.insertFood(new Food(1, name));
         System.out.println(insertedRows + " row(s) inserted");
