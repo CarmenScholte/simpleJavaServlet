c3bb2e676a6448baff46bbedb5a5079965b4ecfd
Carmen Scholte Lubberink <Carmen@BTO.(none)>
2018-03-19 18:45:32 +0100

    Added example Post without params and JSON as a String

--- src/main/java/csl/rest/FoodService.java
+++ src/main/java/csl/rest/FoodService.java
@@ -14,12 +14,12 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
  * Created by Carmen on 18-3-2018.
  */
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/food")
 public class FoodService {
 
     private FoodRepository foodRepository = new FoodRepository();
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value = "",
             method = GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
@@ -27,15 +27,26 @@ public class FoodService {
         return foodRepository.getAllFood();
     }
 
+    // Post with params
     @CrossOrigin(origins = {"*", "http://localhost:4200"})
     @RequestMapping(value = "/newFood",
             method = POST,
             headers = {"Content-Type=application/json"},
             params = {"name"})
-    @ResponseBody
     public void insertFood(@RequestParam("name") String name) {
         int insertedRows = foodRepository.insertFood(new Food(1, name));
         System.out.println(insertedRows + " row(s) inserted");
     }
+
+    // Example without params with String JSON
+//    @CrossOrigin(origins = {"*", "http://localhost:4200"})
+//    @RequestMapping(value = "/newFoodTwo",
+//            method = POST,
+//            headers = {"Content-Type=application/json"})
+//    public void insertFoodTwo(@RequestBody String name) {
+//        System.out.println(name);
+//        int insertedRows = foodRepository.insertFood(new Food(1, name));
+//        System.out.println(insertedRows + " row(s) inserted");
+//    }
 
 }
